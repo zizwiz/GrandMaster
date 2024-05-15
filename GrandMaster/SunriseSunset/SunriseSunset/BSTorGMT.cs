e6ae9a87f725a45fb17085265663ea76c0a4b866
ziz wiz <zizwiz@engineer.com>
2024-05-15 17:54:03 +0100

    Changed the way we work out if we are in British Summer Time

--- GrandMaster/SunriseSunset/SunriseSunset/BSTorGMT.cs
+++ GrandMaster/SunriseSunset/SunriseSunset/BSTorGMT.cs
@@ -1,53 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SunriseSunset
 {
     class CheckDate
     {
-        public static string LastSundayOfMonth(string month, string year)
+        public static DateTime LastSundayOfMonth(string month, string year)
         {
-            //bool result = false;
-
-            //int myYear = Convert.ToInt32(year);
-            //int myMonth = Convert.ToInt32(month);
-
-            //DateTime date;
-            //for (int i = 1; i <= 12; i++)
-            //{
-            //    date = new DateTime(myYear, myMonth, DateTime.DaysInMonth(myYear, myMonth), System.Globalization.CultureInfo.CurrentCulture.Calendar);
-            //    /* Modification by Albert Zakhia on 2021-16-02
-            //       The below code is very slow due to the loop, we will go twice as fast
-            //    while (date.DayOfWeek != DayOfWeek.Sunday)
-            //    {
-            //        date = date.AddDays(-1);
-            //    }
-            //    */
-            //    // The updated code
-            //    int daysOffset = date.DayOfWeek - dayOfWeek; // take the offset to subtract directly instead of looping
-            //    if (daysOffset < 0) daysOffset += 7; // if the code is negative, we need to normalize them
-            //    date = date.AddDays(-daysOffset); // now just add the days offset
-            //    Console.WriteLine(date.ToString("yyyy-MM-dd"));
-            //}
-
             int myyear = Convert.ToInt32(year);
             int mymonth = Convert.ToInt32(month);
             var myDayOfWeek = DayOfWeek.Sunday;
 
-            DateTime date;
-            string answer = "";
-            
-            date = new DateTime(myyear, mymonth, DateTime.DaysInMonth(myyear, mymonth), System.Globalization.CultureInfo.CurrentCulture.Calendar);
+            DateTime date = new DateTime(myyear, mymonth, DateTime.DaysInMonth(myyear, mymonth), System.Globalization.CultureInfo.CurrentCulture.Calendar);
 
             int daysOffset = date.DayOfWeek - myDayOfWeek;
             if (daysOffset < 0) daysOffset += 7; // if the code is negative, we need to normalize them
 
-            answer = date.AddDays(-daysOffset).ToLongDateString();
-            
-            return answer;
+           return DateTime.Parse(date.AddDays(-daysOffset).ToLongDateString());
         }
 
 
--- GrandMaster/SunriseSunset/SunriseSunset/Form1.cs
+++ GrandMaster/SunriseSunset/SunriseSunset/Form1.cs
@@ -39,15 +39,15 @@ namespace SunriseSunset
             int month = SunriseSunsetDateTimePicker.Value.Month;
             int day = SunriseSunsetDateTimePicker.Value.Day;
 
-            bool DaylightSavingOn = IsDaylightSavingOn();
+            //Find last Sunday in March and October of year in datepicker
+            DateTime marchDate = CheckDate.LastSundayOfMonth("3", year.ToString());
+            DateTime octoberDate = CheckDate.LastSundayOfMonth("10", year.ToString());
+            DateTime DateToCheck = SunriseSunsetDateTimePicker.Value;
 
-            //SunriseSunsetDateTimePicker
+            //Are we in BritishSummerTime for chosen year?
+            bool BritishSummerTime = marchDate <= DateToCheck && DateToCheck <= octoberDate;
 
-            //Find last Sunday in March and October
-            var marchDate = CheckDate.LastSundayOfMonth("3", "2024");
-            var octoberDate = CheckDate.LastSundayOfMonth("10", "2024");
 
-            
             ///////////////////////////////////////////////////////////////////////////////
             /// Standard Sunrise and Sunset
             /////////////////////////////////////////////////////////////////////////////// 
@@ -58,12 +58,12 @@ namespace SunriseSunset
             rchtxbx_output.AppendText("Standard Sunrise and Sunset\r");
 
             TimeSpan sunriseTime = TimeSpan.FromHours(tsunrise);
-            if (DaylightSavingOn) sunriseTime = sunriseTime + new TimeSpan(1, 0, 0);
+            if (BritishSummerTime) sunriseTime = sunriseTime + new TimeSpan(1, 0, 0);
             string sunriseTimeString = sunriseTime.ToString(@"hh\:mm\:ss");
             rchtxbx_output.AppendText("Sunrise = " + sunriseTimeString + "\r");
 
             TimeSpan sunsetTime = TimeSpan.FromHours(tsunset);
-            if (DaylightSavingOn) sunsetTime = sunsetTime + new TimeSpan(1, 0, 0);
+            if (BritishSummerTime) sunsetTime = sunsetTime + new TimeSpan(1, 0, 0);
             string sunsetTimeString = sunsetTime.ToString(@"hh\:mm\:ss");
             rchtxbx_output.AppendText("Sunset = " + sunsetTimeString + "\r\r");
             
@@ -76,12 +76,12 @@ namespace SunriseSunset
             rchtxbx_output.AppendText("Civil Twilight\r");
 
             TimeSpan CTsunriseTime = TimeSpan.FromHours(tsunrise);
-            if (DaylightSavingOn) CTsunriseTime = CTsunriseTime + new TimeSpan(1, 0, 0);
+            if (BritishSummerTime) CTsunriseTime = CTsunriseTime + new TimeSpan(1, 0, 0);
             string CTsunriseTimeString = CTsunriseTime.ToString(@"hh\:mm\:ss");
             rchtxbx_output.AppendText("Sunrise = " + CTsunriseTimeString + "\r");
 
             TimeSpan CTsunsetTime = TimeSpan.FromHours(tsunset);
-            if (DaylightSavingOn) CTsunsetTime = CTsunsetTime + new TimeSpan(1, 0, 0);
+            if (BritishSummerTime) CTsunsetTime = CTsunsetTime + new TimeSpan(1, 0, 0);
             string CTsunsetTimeString = CTsunsetTime.ToString(@"hh\:mm\:ss");
             rchtxbx_output.AppendText("Sunset = " + CTsunsetTimeString + "\r\r");
 
@@ -94,12 +94,12 @@ namespace SunriseSunset
             rchtxbx_output.AppendText("Nautical Twilight\r");
 
             TimeSpan NTsunriseTime = TimeSpan.FromHours(tsunrise);
-            if (DaylightSavingOn) NTsunriseTime = NTsunriseTime + new TimeSpan(1, 0, 0);
+            if (BritishSummerTime) NTsunriseTime = NTsunriseTime + new TimeSpan(1, 0, 0);
             string NTsunriseTimeString = NTsunriseTime.ToString(@"hh\:mm\:ss");
             rchtxbx_output.AppendText("Sunrise = " + NTsunriseTimeString + "\r");
 
             TimeSpan NTsunsetTime = TimeSpan.FromHours(tsunset);
-            if (DaylightSavingOn) NTsunsetTime = NTsunsetTime + new TimeSpan(1, 0, 0);
+            if (BritishSummerTime) NTsunsetTime = NTsunsetTime + new TimeSpan(1, 0, 0);
             string NTsunsetTimeString = NTsunsetTime.ToString(@"hh\:mm\:ss");
             rchtxbx_output.AppendText("Sunset = " + NTsunsetTimeString + "\r\r");
 
@@ -114,12 +114,12 @@ namespace SunriseSunset
             rchtxbx_output.AppendText("Astronomical Twilight\r");
 
             TimeSpan ATsunriseTime = TimeSpan.FromHours(tsunrise);
-            if (DaylightSavingOn) ATsunriseTime = ATsunriseTime + new TimeSpan(1, 0, 0);
+            if (BritishSummerTime) ATsunriseTime = ATsunriseTime + new TimeSpan(1, 0, 0);
             string ATsunriseTimeString = ATsunriseTime.ToString(@"hh\:mm\:ss");
             rchtxbx_output.AppendText("Sunrise = " + ATsunriseTimeString + "\r");
 
             TimeSpan ATsunsetTime = TimeSpan.FromHours(tsunset);
-            if (DaylightSavingOn) ATsunsetTime = ATsunsetTime + new TimeSpan(1, 0, 0);
+            if (BritishSummerTime) ATsunsetTime = ATsunsetTime + new TimeSpan(1, 0, 0);
             string ATsunsetTimeString = ATsunsetTime.ToString(@"hh\:mm\:ss");
             rchtxbx_output.AppendText("Sunset = " + ATsunsetTimeString + "\r\r");
         }
@@ -132,38 +132,6 @@ namespace SunriseSunset
         private void btn_reset_Click(object sender, EventArgs e)
         {
             rchtxbx_output.Text = "";
-        }
-
-        private bool IsDaylightSavingOn()
-        {
-            /////////////////////////////////////
-            /// Are we on Daylight saving time?
-            ///////////////////////////////////// 
-            ////
-            /// 
-            //get the current UTC time
-            DateTime localServerTime = DateTime.UtcNow;
-
-            //Find out if the GMT is in daylight saving time or not.
-            var info = TimeZoneInfo.FindSystemTimeZoneById("GMT Standard Time");
-            var isDaylightSaving = info.IsDaylightSavingTime(localServerTime);
-
-            //set the time to be the local server time
-            var correctDateTime = localServerTime;
-
-            //if the zone is in daylight saving (summertime in UK) add an hour.
-            if (isDaylightSaving)
-            {
-                rchtxbx_output.AppendText("DaylightSaving ON\r");
-                correctDateTime = correctDateTime.AddHours(1);
-            }
-
-            //get the UK culture and return the correct date time in the correct format
-            var culture = new CultureInfo("en-GB");
-            rchtxbx_output.AppendText("Time = " + correctDateTime.ToString(culture) + "\r\r");
-
-
-            return isDaylightSaving;
         }
     }
 }
